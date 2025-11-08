fae631217421a0488dcfc195af900e15327a31de
Babunski0 <vasilijeivanisevic@gmail.com>
2025-11-08 14:30:50 +0100

    ADR page
    Add ADR page layout with sidebar and styled sections

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue } from "clsx"
-import clsx from "clsx"
+import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+
+type ClassValue =
+  | string
+  | number
+  | null
+  | undefined
+  | Record<string, boolean>
+  | ClassValue[]
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
