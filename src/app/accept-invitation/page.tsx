24270efb5b340aa58a3c305393ff69f5d944908a
Ewen ANSQUER <ewen@ansquer.fr>
2025-11-09 13:24:04 +0100

    Fix indentation

--- src/app/accept-invitation/page.tsx
+++ src/app/accept-invitation/page.tsx
@@ -5,26 +5,26 @@ import React, {use, useState} from "react";
 import {useRouter} from "next/navigation";
 import {AppRouterInstance} from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-export default function AcceptInvitation() : React.JSX.Element {
-    const router : AppRouterInstance = useRouter();
+export default function AcceptInvitation(): React.JSX.Element {
+    const router: AppRouterInstance = useRouter();
 
     type RegisterFormData = {
-        org: { id : number},
+        org: { id: number },
         name: string,
         username: string,
-        email : string,
+        email: string,
         password: string,
     }
 
     // For the moment, let says everyone is from the same org.
     const [formData, setFormData] = useState<RegisterFormData>({
-        org: { id : 1},
+        org: {id: 1},
         name: "",
         username: "",
         email: "",
         password: "",
     })
-    const [errorMessage, setErrorMessage] = useState<string | null>();
+    const [errorMessage, setErrorMessage] = useState<string | null>();
 
     /**
      * TODO Make this version generic.
@@ -33,7 +33,7 @@ export default function AcceptInvitation() : React.JSX.Element {
      *
      * @alpha
      */
-    async function handleSubmit(e : React.FormEvent<HTMLFormElement>) : Promise<void> {
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>): Promise<void> {
         e.preventDefault(); // Prevent reloading
         try {
             const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/users", {
@@ -41,7 +41,7 @@ export default function AcceptInvitation() : React.JSX.Element {
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify(formData),
             });
-            if (!res.ok){
+            if (!res.ok) {
                 console.log(res.statusText);
                 await res.json().then(data => setErrorMessage(data.message));
                 return;
@@ -71,20 +71,20 @@ export default function AcceptInvitation() : React.JSX.Element {
                         <h2 className="text-3xl font-bold text-center text-black">
                             Register
                         </h2>
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <label className="block text-sm font-semibold text-black mb-1">
-                                Name
-                            </label>
-                            <input
-                                type="text" required
-                                value={formData.name}
-                                onChange={(e) => setFormData({...formData, name: e.target.value})}
-                                className="w-full rounded-[10px] border border-[#5E50A4] px-4 py-3 outline-none focus:ring-2 focus:ring-[#5E50A4]"
-                                placeholder="John"
-                            />
-                        </div>
-                        {/*
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <label className="block text-sm font-semibold text-black mb-1">
+                                    Name
+                                </label>
+                                <input
+                                    type="text" required
+                                    value={formData.name}
+                                    onChange={(e) => setFormData({...formData, name: e.target.value})}
+                                    className="w-full rounded-[10px] border border-[#5E50A4] px-4 py-3 outline-none focus:ring-2 focus:ring-[#5E50A4]"
+                                    placeholder="John"
+                                />
+                            </div>
+                            {/*
                             <div>
                                 <label className="block text-sm font-semibold text-black mb-1">
                                     Username
@@ -98,42 +98,46 @@ export default function AcceptInvitation() : React.JSX.Element {
                                 />
                             </div>
                         */}
-                        <div>
-                            <label className="block text-sm font-semibold text-black mb-1">
-                                Email
-                            </label>
-                            <input
-                                type="email" required
-                                value={formData.email}
-                                onChange={(e) => setFormData({...formData, email: e.target.value, username: e.target.value})}
-                                className="w-full rounded-[10px] border border-[#5E50A4] px-4 py-3 outline-none focus:ring-2 focus:ring-[#5E50A4]"
-                                placeholder="John"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-sm font-semibold text-black mb-1">
-                                Password
-                            </label>
-                            <input
-                                type="password" required
-                                value={formData.password}
-                                onChange={(e) => setFormData({...formData, password: e.target.value})}
-                                className="w-full rounded-[10px] border border-[#5E50A4] px-4 py-3 outline-none focus:ring-2 focus:ring-[#5E50A4]"
-                                placeholder="John"
-                            />
-                        </div>
+                            <div>
+                                <label className="block text-sm font-semibold text-black mb-1">
+                                    Email
+                                </label>
+                                <input
+                                    type="email" required
+                                    value={formData.email}
+                                    onChange={(e) => setFormData({
+                                        ...formData,
+                                        email: e.target.value,
+                                        username: e.target.value
+                                    })}
+                                    className="w-full rounded-[10px] border border-[#5E50A4] px-4 py-3 outline-none focus:ring-2 focus:ring-[#5E50A4]"
+                                    placeholder="John"
+                                />
+                            </div>
+                            <div>
+                                <label className="block text-sm font-semibold text-black mb-1">
+                                    Password
+                                </label>
+                                <input
+                                    type="password" required
+                                    value={formData.password}
+                                    onChange={(e) => setFormData({...formData, password: e.target.value})}
+                                    className="w-full rounded-[10px] border border-[#5E50A4] px-4 py-3 outline-none focus:ring-2 focus:ring-[#5E50A4]"
+                                    placeholder="John"
+                                />
+                            </div>
 
-                        <button
-                            type="submit"
-                            className="mt-2 w-full h-[56px] rounded-[24px] bg-[#5E50A4] text-white"
-                        >
-                            Next
-                        </button>
-                    </form>
+                            <button
+                                type="submit"
+                                className="mt-2 w-full h-[56px] rounded-[24px] bg-[#5E50A4] text-white"
+                            >
+                                Next
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
 
-        )
+    )
 }
