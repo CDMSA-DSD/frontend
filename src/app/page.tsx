595a27f30519051c415fcd1b01a67e254cce34e8
Ewen ANSQUER <ewen@ansquer.fr>
2025-11-09 04:37:52 +0100

    Fixed routes and correct import "next/navigation", not "next/router"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Background from "../components/background"
 import React, {useState} from "react";
-import {useRouter} from "next/router";
+import {useRouter} from "next/navigation";
 
 export default function Home(): React.JSX.Element {
     const [showNewOrganizationModal, setShowNewOrganizationModal] = useState(false);
