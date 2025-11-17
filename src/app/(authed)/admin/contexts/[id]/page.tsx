c7b545af4b9667e53c446233f75df12fd2aa9cde
Ewen ANSQUER <ewen@ansquer.fr>
2025-11-17 23:17:00 +0100

    Created page

---
+++ src/app/(authed)/admin/contexts/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from "react";
+
+export default function getContextById({params} : {params : {id : number }}): React.JSX.Element {
+
+    return (
+        <div>
+            <div className="flex col">
+
+            </div>
+        </div>
+    )
+}
