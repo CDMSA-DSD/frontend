323c1f49062de28f93c7d36fbd260c9256db6b15
Ewen ANSQUER <ewen@ansquer.fr>
2025-12-17 13:17:19 +0100

    Now it uses Microsoft OAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Background from "../components/background"
 import React, {FormEvent, useState} from "react";
 import {useRouter} from "next/navigation";
+import MicrosoftOAuth from "@/components/ui/MicrosoftOAuth";
 
 export default function Home(): React.JSX.Element {
     const [showNewOrganizationModal, setShowNewOrganizationModal] = useState(false);
@@ -460,10 +461,7 @@ export default function Home(): React.JSX.Element {
                             >
                                 Login
                             </button>
-                            <a className="items-center justify-center p-4 flex mt-2 w-full h-[56px] rounded-[24px] bg-[#5E50A4] text-white hover:bg-violet-700 transition-colors" href="https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=3a95e7be-4755-451a-b253-b7bc24004214&response_type=code&redirect_uri=http://localhost:3000/auth/microsoft&response_mode=query&scope=openid%20profile%20email%20https%3A%2F%2Fgraph.microsoft.com%2Fuser.read">
-                                    <img className="mr-4" src="https://learn.microsoft.com/en-us/entra/identity-platform/media/howto-add-branding-in-apps/ms-symbollockup_mssymbol_19.svg"/>
-                                    <p className="">Login with Microsoft Account</p>
-                            </a>
+                            <MicrosoftOAuth/>
                             <div className="flex items-center justify-between text-sm w-full">
                                 <label className="inline-flex items-center gap-2 cursor-pointer select-none">
                                     <input
